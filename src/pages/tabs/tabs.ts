f08522359d73a3f4600bba613e7a44f623dc3b39
nirajnishan27 <nirajnishan27@gmail.com>
2017-08-14 12:50:38 +0530

    first commit

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-
+import { PlacesPage } from '../places/places';
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -12,7 +12,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
-
+  tab4Root = PlacesPage;
   constructor() {
 
   }
